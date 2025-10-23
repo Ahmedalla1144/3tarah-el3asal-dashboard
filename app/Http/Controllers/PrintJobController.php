c4fbdb01c171f3020160d6aaf0b6c20ed42ffb37
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-10-23 18:08:49 +0300

    done

--- app/Http/Controllers/PrintJobController.php
+++ app/Http/Controllers/PrintJobController.php
@@ -13,6 +13,7 @@ class PrintJobController extends Controller
         $job = PrintJob::create([
             'invoice_id' => $request->invoice_id,
             'printer_name' => 'Samsung M332x 382x 402x Series',
+            // 'printer_name' => $request->printer_name ?? 'Samsung Universal Print Driver 2',
             'type' => $request->type ?? 'sales',
         ]);
 
